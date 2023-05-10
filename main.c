5ef89fef54bd6958fc523d3aac28da3aec045dc7
Alvarez0Jose <josealejandro220@gmail.com>
2023-05-10 17:18:40 +0000

    fixing betty problems

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
  * main - entry point
  *@argv: argument vector
  *@argc: argument counter
- *Return: 0 
+ *Return: 0
  */
 
 int main(int argc, char **argv)
--- monty.h
+++ monty.h
@@ -22,9 +22,9 @@
  */
 typedef struct stack_s
 {
-        int n;
-        struct stack_s *prev;
-        struct stack_s *next;
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
 } stack_t;
 
 /**
@@ -37,13 +37,13 @@ typedef struct stack_s
  */
 typedef struct instruction_s
 {
-        char *opcode;
-        void (*f)(stack_t **stack, unsigned int line_number);
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
 /** Prototypes and Helper functions **/
 
-void execute_instruction(char *line, stack_t **stack, unsigned int line_number);
+void execute(char *line, stack_t **stack, unsigned int line_number);
 int is_integer(char *str);
 void push(stack_t **stack, unsigned int line_number, char *arg);
 void pall(stack_t **stack, unsigned int line_number);
@@ -63,7 +63,7 @@ void rotr(stack_t **stack, unsigned int line_number);
 void stack(stack_t **stack, unsigned int line_number);
 void print_stack(stack_t *stack);
 void queue(stack_t **stack, unsigned int line_number);
-void free_stack(stack_t **stack);
+void FreeStack(stack_t **stack);
 
 
 #endif
