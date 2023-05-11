c29d8bb932f69de36959a08163bae68cafd4abd5
Alvarez0Jose <josealejandro220@gmail.com>
2023-05-11 16:13:20 +0000

    adding the command that does nothing

---
+++ monty_nop.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+
+/**
+ * nop - does nothing in the stack
+ * @stack: stack provided in the struct
+ * @line_number: line where the code is
+ */
+
+void nop(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+	(void)line_number;
+}
