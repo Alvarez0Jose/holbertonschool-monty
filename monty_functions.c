d8b5a0347f4e73d92b1032e2664c44947bf70311
Alvarez0Jose <josealejandro220@gmail.com>
2023-05-11 15:23:59 +0000

    adding a , that was missing in two functions

--- monty_functions.c
+++ monty_functions.c
@@ -24,9 +24,9 @@ void execute(char *line, stack_t **stack, unsigned int line_number)
 		pall(stack, line_number);
 	else if (strcmp(opcode, "pint") == 0)
 		pint(stack, line_number);
-	else if (strcmp(opcode "pop") == 0)
+	else if (strcmp(opcode, "pop") == 0)
 		pop(stack, line_number);
-	else if (strcmp(opcode "swap") == 0)
+	else if (strcmp(opcode, "swap") == 0)
 		swap(stack, line_number);
 	else
 	{
